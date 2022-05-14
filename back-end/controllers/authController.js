62b7228cbb8a51742fe99c51eee939aa9be4239a
rae charles <r_charles87@yahoo.com>
2022-05-13 22:55:46 -0400

    added dummy auth

---
+++ back-end/controllers/authController.js
@@ -0,0 +1,27 @@
+// auth.post("/signup", async(req, res) => {
+//     const { body } = req;
+//     try{
+//         const createdCustomer = await createCustomer(body);
+//         if(createdCustomer.id){
+//             res.status(200).json(createdCustomer);
+//         }else{
+//             res.status(500).json({error: 'Cannot create user'});
+//         }
+//     }catch(err){
+//         return err;
+//     }
+// })
+
+// auth.post("/signin", async(req, res) => {
+//     const { body } = req;
+//     try{
+//         const validIdNum = await checkAccount(body.username, body.password);
+//         if(validIdNum){
+//             res.status(200).json(validIdNum);
+//         }else{
+//             res.status(500).json({error: 'Cannot create user'});
+//         }
+//     }catch(err){
+//         return err;
+//     }
+// });
