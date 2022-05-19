aac9a9743321b94e0ef42e84455fb0bf7fd6a085
hinkabi <hinkabi@gmail.com>
2022-05-19 19:09:16 -0400

    Changes to Home.js and home.css

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,35 +1,36 @@
 import veggies from "../assets/veggies.jpg";
 import veggies2 from "../assets/veggies2.jpg";
 import fastfood from "../assets/fast_food.jpeg";
-import greenbag from "../assets/bag.png";
-import standing from "../assets/standing.png";
+import bagicon from "../assets/bag.png";
+import signupicon from "../assets/standing.png";
 import "../Styles/home.css";
 
 let sampleText =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper tincidunt augue, eu rutrum nunc venenatis ut. Donec vel lacinia mi. Sed ac fringilla leo, a luctus ligula. Curabitur diam sapien, consectetur eu convallis placerat, tempor rhoncus lectus. In tempor vehicula vestibulum. In sollicitudin arcu et felis ornare, eu ullamcorper diam rutrum. Morbi sit amet ligula urna. Nunc id varius neque. Praesent sodales tincidunt leo consequat aliquam.Sed ac fringilla leo, a luctus ligula. Curabitur diam sapien, consectetur eu convallis placerat, tempor rhoncus lectus. In tempor vehicula vestibulum.";
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ullamcorper tincidunt augue, eu rutrum nunc venenatis ut. Donec vel lacinia mi. Sed ac fringilla leo, a luctus ligula. Curabitur diam sapien, consectetur eu convallis placerat, tempor rhoncus lectus. In tempor vehicula vestibulum Praesent sodales tincidunt leo consequat aliquam.Sed ac fringilla leo, a luctus ligula. Curabitur diam sapien, consectetur eu convallis placerat, tempor rhoncus lectus. In tempor vehicula vestibulum.";
 
 function Home() {
   return (
-    <div className="home">
-      <img src={fastfood} alt="fastfood" id="fastfood" />
-
-      <div className="links-container">
-          <div className="standing-container">
-          <img src={standing} alt="person on computer" id="standing-img" />
-          <p>Sign Up</p>
-          </div>
-          <div className="faq-container">
-          <img src={greenbag} alt="recycle bag" id="bag-img" />
-          <h6>FAQ</h6>
-          </div>
+    <main>
+      <div id="fast-food-img">
+        <div id="fast-food-text">Meals 4 NYC</div>
       </div>
-
-      <img src={veggies} alt="vegetables" id="img1" />
-
-      <p>{sampleText}</p>
-
-      <img src={veggies2} alt="vegetables" id="img2" />
-    </div>
+      <div id="main-content">
+        <div id="sign-up">
+          <img src={signupicon} alt="illustration of girl on laptop" />
+        </div>
+        <div id="faq">
+          <img src={bagicon} alt="illustration of green recycle bag" />
+        </div>
+        <div id="veggies-img">
+          <img src={veggies} alt="vegetables in a basket" />
+        </div>
+        <div id="main-content-text">{sampleText}</div>
+        <div id="veggies2-img">
+          <img src={veggies2} alt="vegetables in a basket" />
+        </div>
+        <div id="main-content-text2">{sampleText}</div>
+      </div>
+    </main>
   );
 }
 
