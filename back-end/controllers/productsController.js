f874e9f307a2bc9c82ae319604fada1ef4dd350f
hinkabi <hinkabi@gmail.com>
2022-05-20 15:07:08 -0400

    added get restaurants controller and query

--- back-end/controllers/productsController.js
+++ back-end/controllers/productsController.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const products = express.Router();
-const {getAllProducts} = require("../queries/products.js");
+const {getAllProducts, getRestaurantProducts} = require("../queries/products.js");
 
 products.get("/", async(req,res)=>{
     try {
         const allProducts = await getAllProducts()
         res.status(200).json(allProducts);
+    } catch (error) {
+        return error;
+    };
+});
+
+products.get("/:restaurant_id", async(req,res)=>{
+    const { restaurant_id } = req.params;
+    try {
+        const restaurantProducts = await getRestaurantProducts(restaurant_id)
+        res.status(200).json(restaurantProducts);
     } catch (error) {
         return error;
     };
--- back-end/queries/products.js
+++ back-end/queries/products.js
@@ -2,14 +2,21 @@ const db = require("../db/dbConfig.js");
 
 const getAllProducts = async () =>{
     try {
-        const allProducts = await db.any("SELECT * FROM products ");
+        const allProducts = await db.any("SELECT * FROM products");
         return allProducts
     } catch (error) {
         return error
     };
 };
 
+const getRestaurantProducts = async (restaurant_id) => {
+    try {
+        const restaurantProducts = await db.any("SELECT * FROM products WHERE restaurant_id=$1", restaurant_id);
+        return restaurantProducts;
+    } catch (error) {
+        return error;
+    };
+};
 
 
-
-module.exports = { getAllProducts}
+module.exports = { getAllProducts, getRestaurantProducts}
