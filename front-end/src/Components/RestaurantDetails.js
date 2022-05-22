f347dd311b840d75f472a471d8c24ef1767dd480
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-05-22 18:49:58 -0400

    added links

--- front-end/src/Components/RestaurantDetails.js
+++ front-end/src/Components/RestaurantDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Link, useNavigate, useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import axios from "axios";
 import restaurantStockImage from '../assets/restaurantStockImage.jpeg';
 
@@ -8,14 +8,13 @@ const API = process.env.REACT_APP_API_URL;
 function RestaurantDetails() {
     const [restaurant, setRestaurant ] = useState([]);
     const { id } = useParams();
-    const navigate = useNavigate();
     
     useEffect(() => {
         axios.get(`${API}/restaurants/${id}`)
             .then((res) => {
                 setRestaurant(res.data);
             }).catch((err) => console.log(err))
-    }, [id,API])
+    }, [id])
 
   return (
     <div>
@@ -23,7 +22,12 @@ function RestaurantDetails() {
             <div>{restaurant.name}</div>
             <div>{restaurant.cuisine_type}</div>
             <div>{restaurant.add}</div>
-            <img style={{height:'200px'}} src={restaurantStockImage} alt='restaurant stock image'/>
+            <img style={{height:'200px'}} src={restaurantStockImage} alt='restaurant stock'/>
+        </div>
+        <div className='button-link'>
+            <Link to={`/restaurants`}>
+                <button>Go Back</button>
+            </Link>
         </div>
     </div>
   )
