2aca5c5da2c21aee1250ac289a1fa3dac2614a14
hinkabi <hinkabi@gmail.com>
2022-05-24 20:17:32 -0400

    front end changes to Cart.js and added css

--- front-end/src/Components/Cart.js
+++ front-end/src/Components/Cart.js
@@ -1,40 +1,54 @@
+import "../Styles/cart.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
+
 const API = process.env.REACT_APP_API_URL;
 
-function Cart({carts, setCarts}) {
+function Cart({ carts, setCarts }) {
   // const [carts, setCarts] = useState([]);
 
-    const userID =localStorage.getItem("userID")
+  const userID = localStorage.getItem("userID");
 
+  useEffect(() => {
+    axios
+      .get(`${API}/carts/${userID}/active`)
+      .then((res) => {
+        // console.log(res.data)
+        setCarts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [userID]);
 
-    useEffect(()=>{
-        axios.get(`${API}/carts/${userID}/active`)
-        .then((res)=>{
-          // console.log(res.data)
-           setCarts(res.data);
-        }).catch((err)=>{
-            console.log(err);
-        })
-    }, [userID]);
-
-    const activeCart = carts.map((product)=>{
-      return (
+  const activeCart = carts.map((product) => {
+    return (
+      <div>
+        <img
+          id="food-icon"
+          src={"https://i.imgur.com/JRd96AZ.png"}
+          alt="food icon"
+        />
+        <div>Order: #{product.orderNumber}</div>
+        <div>Restaurant: {product.restaurant}</div>
         <div>
-          {product.orderNumber}
-          {product.restaurant}
           {product.items.map((item) => {
-            return (<div>{item.name} {item.quantity}</div>)
+            return (
+              <div>
+                Meal: {item.name} {item.quantity}
+              </div>
+            );
           })}
         </div>
-      )
-    })
+      </div>
+    );
+  });
 
   return (
-      <div>
-        {activeCart}
-      </div>
-  )
+    <div className="cart-container">
+      <div id="active-cart">{activeCart}</div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
