4f6e802b043045c7d8731a326e80277737219429
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-05-29 00:24:19 -0400

    trying to validate an order from only one restaurant per order

--- back-end/queries/validation.js
+++ back-end/queries/validation.js
@@ -4,7 +4,7 @@ const {createNewCart} = require("../queries/carts.js");
 const validateActiveCart = async (customer_id) => {
     try{
         const customerCartArray = await db.any("SELECT * FROM carts WHERE id=$1 and is_active=TRUE", customer_id);
-        console.log(customerCartArray, "Inside query")
+        // console.log(customerCartArray, "Inside query")
         // [ { id: 2, is_active: true, customer_id: 2 } ] Inside query
         if(customerCartArray.length < 1){ //if the length is 0
             //create a cart for that customer
@@ -19,11 +19,64 @@ const validateActiveCart = async (customer_id) => {
 }
 
 const validateMultipleRestaurants = async (body, activeCart) => {
+    // console.log("inside the second validation function", body, activeCart)
+    console.log("active",activeCart[0].id)
     try{
-        const activeCartDetailArr = await db.any("SELECT * FROM order_details WHERE id=$1", cart.id);
-        console.log(body, activeCart)
-    }catch(err){
+        const activeCart_id = activeCart[0].id
+        const activeOrderDetailsArr = await db.any("SELECT * FROM order_details WHERE carts_id=$1", activeCart_id)   
+        // [
+        //     { carts_id: 2, products_id: 3, quantity: 6 },
+        //     { carts_id: 2, products_id: 16, quantity: 1 },
+        //     { carts_id: 2, products_id: 8, quantity: 1 },
+        //     { carts_id: 2, products_id: 7, quantity: 1 },
+        //     { carts_id: 2, products_id: 19, quantity: 1 },
+        //     { carts_id: 2, products_id: 4, quantity: 1 },
+        //     { carts_id: 2, products_id: 15, quantity: 1 },
+        //     { carts_id: 2, products_id: 1, quantity: 1 },
+        //     { carts_id: 2, products_id: 18, quantity: 1 },
+        //     { carts_id: 2, products_id: 13, quantity: 1 },
+        //     { carts_id: 2, products_id: 2, quantity: 1 }
+        //   ]
+        const productsArr = [];
+        // console.log("AYOO ",activeOrderDetailsArr)
+        for(let productDetail of activeOrderDetailsArr){
+            const productInfo = await db.one("SELECT * FROM products WHERE id=$1", productDetail.products_id);
+            productsArr.push(productInfo)
+        }
+        // console.log("CONSOLE",productsArr)
+        // [
+        //     {
+        //       id: 3,
+        //       name: 'Lactose free',
+        //       description: 'Wild Rice with a side of steamed vegetables',
+        //       portion: '10oz',
+        //       calories: 170,
+        //       quantity_in_stock: 10,
+        //       restaurant_id: 1
+        //     },
+        //     {
+        //       id: 7,
+        //       name: 'Non-dairy',
+        //       description: 'Vibrant Greens with Quinoa',
+        //       portion: '12oz',
+        //       calories: 280,
+        //       quantity_in_stock: 3,
+        //       restaurant_id: 2
+        //     },
+        //     {
+        //       id: 19,
+        //       name: 'Non-dairy',
+        //       description: 'Vibrant Greens with Quinoa',
+        //       portion: '12oz',
+        //       calories: 280,
+        //       quantity_in_stock: 36,
+        //       restaurant_id: 8
+        //     },
+        //   ]
 
+        
+    }catch(err){
+        return err;
     }
 }
 
--- back-end/validations/cartValidation.js
+++ back-end/validations/cartValidation.js
@@ -2,13 +2,14 @@
 const { validateActiveCart, validateMultipleRestaurants } = require("../queries/validation.js");
 
 const validationAddToCart = async (req, res, next) => {
+    console.log("Middlewareis working")
     const { body } = req;
     try{
         const activeCart = await validateActiveCart(body.userID) //ask if the useR has an active cart or not?
         const checkRestaurant = await validateMultipleRestaurants(body, activeCart) //check if the user is adding something from a different restaurant
-       
+    //    console.log(checkRestaurant)
     }catch(err){
-        return err
+        return err;
     }
     next();
 }
