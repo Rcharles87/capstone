1cc0170a010af6a35ef1fbd5cbb368fd631d94ed
rae charles <r_charles87@yahoo.com>
2022-05-30 10:21:06 -0400

    added smalll style changes to profile and cart pages

--- front-end/src/Components/FloatingLinks.js
+++ front-end/src/Components/FloatingLinks.js
@@ -3,6 +3,7 @@ import veggies from "../assets/veggies.jpg";
 import veggies2 from "../assets/veggies2.jpg";
 import bagicon from "../assets/bag.png";
 import signupicon from "../assets/standing.png";
+import {Link} from 'react-router-dom'
 import"../Styles/floatingLinks.css"
 
 
@@ -16,9 +17,15 @@ function FloatingLinks() {
         <div id="main-content">
         <div id="sign-up">
           <img src={signupicon} alt="illustration of girl on laptop" />
+          <Link to="/signup">
+          <h3>Dont make it a tuff decision <br /> signup today!!!</h3>
+          </Link>
         </div>
         <div id="faq">
           <img src={bagicon} alt="illustration of green recycle bag" />
+          <Link to="faq">
+            <h3>Check out our faq's,<br />find out more about food waste,<br /> and how you can help</h3>
+          </Link>
         </div>
         <div id="veggies-img">
           <img src={veggies} alt="vegetables in a basket" />
