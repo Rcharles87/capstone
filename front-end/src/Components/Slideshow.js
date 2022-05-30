1659eee72427baa17e4955501088145e0123493d
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-05-30 01:17:26 -0400

    added Slideshow to home page/Edited Floating links  style sheet

--- front-end/src/Components/Slideshow.js
+++ front-end/src/Components/Slideshow.js
@@ -11,8 +11,8 @@ const Slideshow = () => {
     <span>Baked Goodness</span>
   </div>
   <div class="box">
-    <img src="https://source.unsplash.com/1000x802"></img>
-    <span>Deli Bites</span>
+    <img src="https://www.qwick.com/wp-content/uploads/2021/06/Start-Staff-a-Delivery-Only-Restaurant.jpeg"></img>
+    <span>Quality</span>
   </div>
   <div class="box">
     <img src="https://purewows3.imgix.net/images/articles/2020_04/Brownsville_Community_Culinary_Center_and_Collective_Fare_meal_kit.png"></img>
